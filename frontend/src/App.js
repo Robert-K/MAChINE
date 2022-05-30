a529440e435d3cf4a4aa8759610e6cb887217733
Robert <github@kosro.de>
2022-05-30 14:18:26 +0200

    ADD: React router implementation (navbar)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,8 @@ import {
 import Score from './ScoreComponent.js'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { LinkContainer } from 'react-router-bootstrap'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   // api.getUserGreeting('James')
@@ -22,76 +24,91 @@ function App() {
   }
   return (
     <div className="App">
-      <Navbar
-        className="navbar navbar-dark bg-primary navbar-expand-lg"
-        expand="md"
-        variant="dark"
-        bg="primary"
-      >
-        <Container fluid>
-          <Navbar.Brand href="#home">
-            <img
-              src={logo}
-              width="30"
-              height="30"
-              alt="logo"
-              className="mx-3"
-            />
-            MAChINE
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something else
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#coffee">
-                <FontAwesomeIcon icon={faCoffee} className="me-2" />
-                Coffee
-              </Nav.Link>
-            </Nav>
-            <Form className="d-flex ms-3">
-              <FormControl
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <BrowserRouter>
+        <Navbar
+          className="navbar navbar-dark bg-primary navbar-expand-lg"
+          expand="md"
+          variant="dark"
+          bg="primary"
         >
-          {displayText}
-        </a>
-        <br></br>
-        <Score sendTextToParent={receiveText} />
-      </header>
+          <Container fluid>
+            <Navbar.Brand href="#home">
+              <img
+                src={logo}
+                width="30"
+                height="30"
+                alt="logo"
+                className="mx-3"
+              />
+              MAChINE
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <LinkContainer to="/">
+                  <Nav.Link>Home</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/molecule">
+                  <Nav.Link>Molecule</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/dataset">
+                  <Nav.Link>Dataset</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/train">
+                  <Nav.Link>Train</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/results">
+                  <Nav.Link>Results</Nav.Link>
+                </LinkContainer>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something else
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Nav>
+                <Nav.Link href="#coffee">
+                  <FontAwesomeIcon icon={faCoffee} className="me-2" />
+                  Coffee
+                </Nav.Link>
+              </Nav>
+              <Form className="d-flex ms-3">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {displayText}
+          </a>
+          <br></br>
+          <Score sendTextToParent={receiveText} />
+        </header>
+      </BrowserRouter>
     </div>
   )
 }
