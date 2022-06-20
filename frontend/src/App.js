a783624496324f61b4972bbf785b8e3b2e819da3
Iva Andreeva <iva.andreeva.13@gmail.com>
2022-06-20 16:18:43 +0200

    ADD: theming using ThemeProvider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,23 +7,35 @@ import Swagger from './routes/Swagger'
 
 import '@fontsource/roboto'
 import Navbar from './components/Navbar'
+import { createTheme, ThemeProvider } from '@mui/material'
+import { red } from '@mui/material/colors'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: red[500],
+    },
+  },
+})
 
 function App() {
   // api.getUserGreeting('James')
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Start />}></Route>
-          <Route path="/home" element={<Start />}></Route>
-          <Route path="/models" element={<Start />}></Route>
-          <Route path="/molecules" element={<Start />}></Route>
-          <Route path="/results" element={<Scoreboards />}></Route>
-          <Route path="/swagger" element={<Swagger />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Start />}></Route>
+            <Route path="/home" element={<Start />}></Route>
+            <Route path="/models" element={<Start />}></Route>
+            <Route path="/molecules" element={<Start />}></Route>
+            <Route path="/results" element={<Scoreboards />}></Route>
+            <Route path="/swagger" element={<Swagger />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   )
 }
