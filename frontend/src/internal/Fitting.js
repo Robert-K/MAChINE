8357879604f50086aeeb9c6026b08683672226a1
Iva Andreeva <iva.andreeva.13@gmail.com>
2022-07-06 20:19:59 +0200

    UPD: fixed residual issues and uniform naming scheme for ModelConfig and Fitting

--- frontend/src/internal/Fitting.js
+++ frontend/src/internal/Fitting.js
@@ -1,61 +1,18 @@
-// Model training process
-import { Collapse, Divider, List, ListItem } from '@mui/material'
-import React from 'react'
-import Button from '@mui/material/Button'
-
-class Fitting extends React.Component {
-  constructor(model, dataset, epochs, accuracy) {
-    super([model, dataset, epochs, accuracy])
-    // Name of fitted model, string
-    this.model = model
-    // Dataset identifier, string
-    this.dataset = dataset
-    // Amount of epochs model was trained for, integer
+class Fitting {
+  /**
+   * A Fitting models a training result and consists of
+   * @param id string in format of: '{model}{integer}'
+   * @param modelID of used ModelConfig, string
+   * @param datasetID of used dataset, string
+   * @param epochs , integer
+   * @param accuracy , float between 0 and 100
+   */
+  constructor(id, modelID, datasetID, epochs, accuracy) {
+    this.id = id
+    this.modelID = modelID
+    this.datasetID = datasetID
     this.epochs = epochs
-    // Accuracy of fitting, integer
     this.accuracy = accuracy
-    this.state = { open: false }
-    // this.toggleOpen = this.toggleOpen.bind(this)
-  }
-
-  componentDidMount() {
-    console.log('fitting mounted')
-  }
-
-  // toggleOpen() {}
-
-  render() {
-    // TODO: separate data from rendering, call rendering component and pass data
-    // const [open, setOpen] = React.useState(false)
-    return (
-      <ListItem>
-        {/* TODO: connect style to theme */}
-        <style>{`
-          .listKey {
-            font-weight: bold;
-            color: red;
-          }
-        `}</style>
-        <Button onClick={() => this.setState({ open: !this.state.open })}>
-          <div className="listKey"> {this.dataset} </div>
-        </Button>
-        <Collapse in={this.state.open}>
-          <List>
-            <ListItem>
-              <div className="listKey">Epochs: </div> {this.epochs}
-            </ListItem>
-            <ListItem>
-              <div className="listKey">Accuracy: </div> {this.accuracy}%
-            </ListItem>
-          </List>
-        </Collapse>
-        <Divider />
-      </ListItem>
-    )
-  }
-
-  toString() {
-    return `${this.model} ${this.dataset} ${this.epochs} ${this.accuracy}`
   }
 }
 export default Fitting
--- frontend/src/internal/ModelConfig.js
+++ frontend/src/internal/ModelConfig.js
@@ -1,5 +1,4 @@
 import Fitting from './Fitting'
-import ReactDOM from 'react'
 
 class ModelConfig {
   /**
@@ -15,29 +14,29 @@ class ModelConfig {
     this.name = name
     this.baseModel = baseModelId
     this.parameters = parameters
-    this.fittings = fittings
+    this.fittings = Array.isArray(fittings) ? fittings : []
   }
 
   /**
    * adds a new fitting based on this model configuration
    * generates fitting id using number of present fittings
-   * @param datasetId string identifying the used dataset
+   * @param datasetID string identifying the used dataset
    * @param epochs integer
    * @param accuracy float between 0 and 100
    */
-  addFitting(datasetId, epochs, accuracy) {
+  addFitting(datasetID, epochs, accuracy) {
     const fittingProps = {
       id: `${this.name}${
         !Array.isArray(this.fittings) || !this.fittings.length
           ? 0
           : this.fittings[this.fittings.length - 1].id + 1
       }`,
-      model: this,
-      dataset: datasetId,
+      modelID: this.id,
+      datasetID,
       epochs,
       accuracy,
     }
-    this.fittings.push(ReactDOM.createElement(Fitting, fittingProps, null))
+    this.fittings.push(new Fitting(fittingProps))
     // TODO: initiate creation of backend equivalent
   }
 }
