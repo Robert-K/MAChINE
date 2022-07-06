9df98d2a8ce161b2b0d029b83b397550d4f1a741
Max <max@ortius-illingen.de>
2022-07-06 17:36:23 +0200

    ADD placeholder training Page

--- frontend/src/routes/TrainingPage.js
+++ frontend/src/routes/TrainingPage.js
@@ -1,9 +1,55 @@
 import React from 'react'
+import { Box, Card, CardContent, Divider, Grid, List } from '@mui/material'
+import Button from '@mui/material/Button'
 import { useLocation } from 'react-router-dom'
 
 export default function TrainingPage() {
   const { state } = useLocation()
   const { selectedModel, selectedDatasetID, selectedLabels } = state
   console.log(selectedModel, selectedDatasetID, selectedLabels)
-  return <div>This is the Page for Training</div>
+  return (
+    <List>
+      <List item>
+        <Grid container>
+          <Grid item xs sx={{ maxWidth: 400 }}>
+            <Box>
+              <Card sx={{ minWidth: 250, m: 6 }}>
+                <CardContent sx={{ fontSize: 20 }}>Modell Details</CardContent>
+              </Card>
+              <Card sx={{ minWidth: 250, m: 6 }}>
+                <CardContent sx={{ fontSize: 20 }}>Dataset Details</CardContent>
+              </Card>
+            </Box>
+          </Grid>
+          <Divider
+            oriantation="vertical"
+            variant="middle"
+            flexitem
+            sx={{ border: 'black' }}
+          ></Divider>
+          <Grid item xs>
+            <Box sx={{ m: 5, display: 'flex', justifyContent: 'center' }}>
+              <Box
+                sx={{
+                  width: 600,
+                  height: 500,
+                  background: 'black',
+                }}
+              ></Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </List>
+      <List item>
+        <Grid Item>
+          <Box sx={{ display: 'flex', justifyContent: 'right' }}>
+            <Box>
+              <Button>Start/Stopp</Button>
+              <Button>Continue to Molecule</Button>
+            </Box>
+          </Box>
+        </Grid>
+      </List>
+    </List>
+  )
 }
