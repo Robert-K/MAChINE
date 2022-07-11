1fbc45fa1bb441c5820912d909f5b868d0980108
Iva Andreeva <iva.andreeva.13@gmail.com>
2022-07-11 11:37:50 +0200

    UPD: implemented fitting rendering per model

--- frontend/src/routes/ModelsPage.js
+++ frontend/src/routes/ModelsPage.js
@@ -5,25 +5,35 @@ import {
   CardActions,
   CardContent,
   CardHeader,
+  Collapse,
+  Divider,
   Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
   Typography,
 } from '@mui/material'
 import Button from '@mui/material/Button'
 import ModelConfig from '../internal/ModelConfig'
 import SelectionList from '../components/SelectionList'
 import CollapsibleMenu from '../components/testToggleState'
+// import PropTypes from 'prop-types'
+import ExpandLess from '@mui/icons-material/ExpandLess'
+import ExpandMore from '@mui/icons-material/ExpandMore'
+import PropTypes from 'prop-types'
 
 export default function ModelsPage() {
   const [selectedModel, setSelectedModel] = React.useState('')
 
   const models = [
-    new ModelConfig('Test Model1', '13', [3, 4, 5]),
-    new ModelConfig('Test Model2', '14', [5, 4, 5]),
+    new ModelConfig('1', 'Test Model1', '13', [3, 4, 5], []),
+    new ModelConfig('2', 'Test Model2', '14', [5, 4, 5], []),
   ]
-  models[0].addFitting('dataset1a', 9, 70)
-  models[0].addFitting('dataset1b', 10, 72)
-  models[1].addFitting('dataset2a', 11, 75)
-  models[1].addFitting('dataset2b', 12, 78)
+  models[0].addFitting('1a', 9, 70)
+  models[0].addFitting('1b', 10, 72)
+  models[1].addFitting('2a', 11, 75)
+  models[1].addFitting('2b', 12, 78)
 
   const updateSelection = (index) => {
     setSelectedModel(index)
@@ -75,11 +85,7 @@ export default function ModelsPage() {
               Base Model: {models[getCurrentIndex()].baseModel}
             </Typography>
             <Typography>Trained on: </Typography>
-            {models[getCurrentIndex()].fittings.map((fitting) => (
-              <React.Fragment key={fitting.toString()}>
-                {fitting.render}
-              </React.Fragment>
-            ))}
+            {renderFittings(models[getCurrentIndex()].fittings)}
           </CardContent>
           <CardActions>
             <Grid container justifyContent="center">
@@ -90,4 +96,64 @@ export default function ModelsPage() {
       )
     }
   }
+}
+
+function renderFittings(fittings) {
+  return (
+    <List>
+      <style>{`
+          .listKey {
+            font-weight: bold;
+            color: red;
+          }
+        `}</style>
+      {fittings.map((fitting) => (
+        <RenderFitting fitting={fitting} key={fitting.id}></RenderFitting>
+      ))}
+    </List>
+  )
+}
+
+function RenderFitting(props) {
+  const [open, setOpen] = React.useState(false)
+  const toggleOpen = () => {
+    setOpen(!open)
+  }
+  return (
+    <ListItem key={props.fitting.id}>
+      <ListItemButton onClick={() => toggleOpen()}>
+        {open ? <ExpandLess /> : <ExpandMore />}
+        <ListItemText
+          className="listKey"
+          primary={props.fitting.datasetID}
+        ></ListItemText>
+      </ListItemButton>
+      <Collapse
+        in={open}
+        timeout="auto"
+        mountOnEnter
+        unmountOnExit
+        orientation="vertical"
+      >
+        <List sx={{ pl: 4 }}>
+          <ListItem>
+            <ListItemText className="listKey" primary="Epochs: "></ListItemText>
+            {props.fitting.epochs}
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              className="listKey"
+              primary="Accuracy:"
+            ></ListItemText>
+            {props.fitting.accuracy}%
+          </ListItem>
+        </List>
+      </Collapse>
+      <Divider />
+    </ListItem>
+  )
+}
+
+RenderFitting.propTypes = {
+  fitting: PropTypes.object.isRequired,
 }
