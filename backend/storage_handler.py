938fa19a25a23211e97d5ad987f1140c9559acae
Martin <19313511+CSCMe@users.noreply.github.com>
2022-07-18 16:00:52 +0200

    UPD: Made StorageHandler a singleton
    also updated storage location and .gitignore to reflect that

--- backend/storage_handler.py
+++ backend/storage_handler.py
@@ -3,28 +3,100 @@ import json
 import shutil
 import pickle
 
-# New StorageHandler is created on login, if user folder is present, load that folder, else create new
-# TODO: Delete folder on correct close of program/dereference of Handler. Problem: Scoreboard across different users
+__all__ = ['add_fitting',
+           'add_model',
+           'add_molecule',
+           'delete_user',
+           'get_analyses',
+           'get_base_model',
+           'get_base_model_type',
+           'get_base_model_types',
+           'get_base_models',
+           'get_dataset_info',
+           'get_datasets_info',
+           'get_fitting',
+           'get_fittings',
+           'get_model',
+           'get_models',
+           'get_molecules',
+           'get_or_add_user']
 
-user_data_path = 'storage\\user_data'
-datasets_path = 'storage\\data'
+_user_data_path = 'storage\\user_data'
+_datasets_path = 'storage\\data'
 
 
 class StorageHandler:
 
     def __init__(self):
-        self.__analyze_datasets()
         self.user_storage_handler = dict()
         self.datasets_info = dict()
+        self.base_models = dict()
         self.base_model_types = dict()
+        self.__analyze_datasets()
 
-    def __get_user_handler(self, user_id):
-        handler = self.user_storage_handler.get(user_id)
-        # TODO: Test, may lead to more errors than simply returning 'None'
-        return handler if handler is not None else UserDataStorageHandler('')
+    def get_or_add_user(self, user_id):  # TODO: Test
+        if self.user_storage_handler.get(user_id) is None:
+            self.user_storage_handler[user_id] = UserDataStorageHandler(user_id)
+        return self.user_storage_handler.get(user_id)
 
+    def delete_user(self, user_id):
+        self.user_storage_handler.pop(user_id)
+
+    # Datasets
+    def get_dataset_info(self, dataset_id):
+        return self.datasets_info.get(dataset_id)
+
+    def get_datasets_info(self):
+        return self.datasets_info
+
+    # Base Models
+    def get_base_model(self, base_model_id):
+        return self.base_models.get(base_model_id)
+
+    def get_base_models(self):
+        return self.base_models
+
+    # Base Model Types
+    def get_base_model_type(self, type_name):
+        return self.base_model_types.get(type_name)
+
+    def get_base_model_types(self):
+        return self.base_model_types
+
+    # User Storage
+    # Molecules
+    def add_molecule(self, user_id, smiles, analysis):
+        self.get_or_add_user(user_id).add_new_molecule(smiles, analysis)
+
+    def get_analyses(self, user_id, smiles):
+        self.get_or_add_user(user_id).get_analyses(smiles)
+
+    def get_molecules(self, user_id):
+        self.get_or_add_user(user_id).get_molecules()
+
+    # Models
+    def add_model(self, user_id, model):
+        self.get_or_add_user(user_id).add_model(model)
+
+    def get_model(self, user_id, model_id):
+        self.get_or_add_user(user_id).get_model(model_id)
+
+    def get_models(self, user_id):
+        self.get_or_add_user(user_id).get_models()
+
+    # Fittings
+    def add_fitting(self, user_id, fitting):
+        self.get_or_add_user(user_id).add_fitting(fitting)
+
+    def get_fitting(self, user_id, fitting_id):
+        self.get_or_add_user(user_id).get_fitting(fitting_id)
+
+    def get_fittings(self, user_id):
+        self.get_or_add_user(user_id).get_fittings()
+
+    # Private Methods
     def __analyze_datasets(self):
-        for idx, dataset_path in enumerate(sorted((Path.cwd() / datasets_path).glob('*.pkl'))):
+        for idx, dataset_path in enumerate(sorted((Path.cwd() / _datasets_path).glob('*.pkl'))):
             if dataset_path.exists():
                 self.datasets_info[idx] = self.__parse_dataset_info(dataset_path)
 
@@ -37,37 +109,21 @@ class StorageHandler:
         dataset_info = {'name': str(labels.keys),
                         'size': len(dataset),
                         'labelDescriptors': list(labels.keys()),
-                        'datasetPath': ''}
+                        'datasetPath': str(dataset_path.absolute())}
         return dataset_info
 
 
 class UserDataStorageHandler:
 
     def __init__(self, user_id):
-        self.models = {}
-        self.molecules = {'aaa': {5: {'god_why': 'help', 'number': 42, 'true': False}}} # TODO: delete test data
-        self.fittings = {}
-        self.base_models = {}
-        self.datasets = {}
-        self.user_id = user_id
-        self.user_path = Path.cwd() / user_data_path / user_id
-        load_saved = self.user_path.exists()
+        self.user_path = Path.cwd() / _user_data_path / user_id
         self.__build_folder_structure()
-        if load_saved:  # reload last saved data
-            # self.models = self.__load_models()
-            self.molecules = self.__load_file('molecules.json')
-            # self.fittings = self.__load_fittings()
-        else:  # create all new files
-
-            # self.__save_model_file()
-            self.__save_file('molecules.json', self.molecules)
-            # self.__save_fittings_file()
-
-        # self.datasets = self.__parse_datasets(Path.cwd() / 'data')
+        self.models = self.__load_file('models.json')
+        self.molecules = self.__load_file('molecules.json')
+        self.fittings = self.__load_file('fittings.json')
 
     def __del__(self):
-        # self.__clean_files()
-        pass
+        self.__clean_files()
 
     def add_new_molecule(self, smiles, analysis):
         self.molecules[smiles] = analysis
@@ -79,26 +135,21 @@ class UserDataStorageHandler:
     def get_fittings(self):
         return self.fittings
 
-    def get_model_name_from_fitting(self, fitting_id):
-        return self.models[self.fittings[fitting_id]["model_id"]]["name"]
-
-    # @param molecule: smiles code
-    def get_analyses(self, molecule):
-        return self.molecules[molecule]
+    def get_analyses(self, smiles):
+        return self.molecules[smiles]
 
     def get_models(self):
         return self.models
 
-    def get_datasets(self):
-        return self.datasets
-
+    def __get_model_name_from_fitting(self, fitting_id):
+        return self.models[self.fittings[fitting_id]["model_id"]]["name"]
 
     def __load_file(self, filename):
         content = dict()
         file_path = (self.user_path / filename)
         if file_path.exists():
             file = file_path.open('r')
-            array = json.load(file)
+            content = json.load(file)
             file.close()
         return content
 
@@ -113,3 +164,23 @@ class UserDataStorageHandler:
 
     def __build_folder_structure(self):
         self.user_path.mkdir(parents=True, exist_ok=True)
+
+
+_inst = StorageHandler()
+add_fitting = _inst.add_fitting
+add_model = _inst.add_model
+add_molecule = _inst.add_molecule
+delete_user = _inst.delete_user
+get_analyses = _inst.get_analyses
+get_base_model = _inst.get_base_model
+get_base_model_type = _inst.get_base_model_type
+get_base_model_types = _inst.get_base_model_types
+get_base_models = _inst.get_base_models
+get_dataset_info = _inst.get_dataset_info
+get_datasets_info = _inst.get_datasets_info
+get_fitting = _inst.get_fitting
+get_fittings = _inst.get_fittings
+get_model = _inst.get_model
+get_models = _inst.get_models
+get_molecules = _inst.get_molecules
+get_or_add_user = _inst.get_or_add_user
