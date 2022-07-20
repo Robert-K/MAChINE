7c1977d75cb5695bf88ff671da424508a2b3a50b
Martin <19313511+CSCMe@users.noreply.github.com>
2022-07-20 14:44:49 +0200

    ADD: Backend API methods & stubs

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,6 @@
-from flask import Flask, request
+from flask import Flask
 from flask_cors import CORS
-from flask_restful import reqparse, abort, Api, Resource
+from flask_restful import reqparse, Api, Resource
 
 from backend import storage_handler as sh
 
@@ -8,81 +8,72 @@ app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-models = {
-    'model1': {'description': 'build an API'},
-    'model2': {'description': '?????'},
-    'model3': {'description': 'profit!'},
-}
-
-
-def abort_if_model_doesnt_exist(model_id):
-    if model_id not in models:
-        abort(404, message="model {} doesn't exist".format(model_id))
-
-
 parser = reqparse.RequestParser()
 parser.add_argument('task')
-
-
-# model
-# shows a single model item and lets you delete a model item
-class Model(Resource):
-    def get(self, model_id):
-        abort_if_model_doesnt_exist(model_id)
-        return sh.get_model_config(model_id)
-
-    def delete(self, model_id):
-        abort_if_model_doesnt_exist(model_id)
-        del models[model_id]
-        return '', 204
-
-    def put(self, model_id):
-        args = parser.parse_args()
-        task = {'task': args['task']}
-        models[model_id] = task
-        return task, 201
 
 
 # modelList
 # shows a list of all models, and lets you POST to add new tasks
 class Models(Resource):
+    def get(self, user_id):
+        return sh.get_models(user_id)
+
+    def post(self, user_id):
+        pass  # TODO: implement
+
+
+class Molecules(Resource):
+    def get(self, user_id):
+        return sh.get_molecules(user_id)
+
+    def post(self, user_id):
+        pass  # TODO: implement
+
+
+class Fittings(Resource):
+    def get(self, user_id):
+        return sh.get_fittings(user_id)
+
+
+class Users(Resource):
+    def post(self, user_id):
+        return sh.get_or_add_user(user_id)
+
+    def delete(self, user_id):
+        return sh.delete_user(user_id)
+
+
+class Datasets(Resource):
     def get(self):
-        return models
-
-    def post(self):
-        args = parser.parse_args()
-        model_id = int(max(models.keys()).lstrip('model')) + 1
-        model_id = 'model%i' % model_id
-        models[model_id] = {'task': args['task']}
-        return models[model_id], 201
+        return sh.get_datasets_info()
 
 
-class ModelList(Resource):
+class BaseModels(Resource):
     def get(self):
-        return sh.get_models()
+        return sh.get_base_models
 
 
-class MoleculeList(Resource):
-    def get(self):
-        return sh.get_molecules()
+class Analyze(Resource):
+    def post(self, user_id):
+        pass  # TODO: Implement
 
 
-class AddModel(Resource):
-    def post(self):
-        json_data = request.get_json()
-        return {"modelconfig_ID": sh.add_new_model_config(json_data['num_layers']['numlayers'],
-                                                          json_data['units_per_layer']['unitsperlayer'])}
+class Train(Resource):
+    def post(self, user_id):
+        pass  # TODO: Implements
 
 
-##
-## Actually setup the Api resource routing here
-##
-api.add_resource(Models, '/models')
-api.add_resource(AddModel, '/models/add')
-api.add_resource(ModelList, '/modellist')  # returns a list of all used models with parameters, used for scoreboard
-api.add_resource(MoleculeList,
-                 '/moleculelist')  # returns a list of all used molecules with properties, used for scoreboard
-api.add_resource(Model, '/models/<model_id>')
+# Actually set up the Api resource routing here
+api.add_resource(Users, '/users/<user_id>')
+api.add_resource(Models, '/users/<user_id>/models')
+api.add_resource(Molecules, '/users/<user_id>/molecules')
+api.add_resource(Fittings, '/users/<user_id>/fittings')
+# Training & Analyzing
+api.add_resource(Analyze, '/users/<user_id>/analyze')
+api.add_resource(Train, '/user/<user_id>/train')
+# Non-user-specific resources
+api.add_resource(Datasets, '/datasets')
+api.add_resource(BaseModels, '/baseModels')
 
 
 def run(debug=True):
@@ -90,4 +81,7 @@ def run(debug=True):
 
 
 if __name__ == '__main__':
+    test_user = 'yee'
+    sh.get_or_add_user(test_user)
+    sh.add_molecule(test_user, 'aaah', {5: {'god_why': 'help', 'number': 42, 'true': False}})  # For testing purposes
     run()
