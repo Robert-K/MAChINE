7b7a5b880103b30bd53dada2e1dc4efd5b13b870
Viola Buck Cabrera <83520648+Viola2345@users.noreply.github.com>
2022-07-25 16:27:18 +0200

    UPD: Rewriting model.py by replacing it with train and analyze

--- backend/model.py
+++ backend/model.py
@@ -1,54 +1,19 @@
-# inspired by André
-import json
-import pickle
-
-import numpy as np
-import tensorflow as tf
-
-# n_units_per_layer = 256
-# fingerprint_size = 128
-# label = "homo"
-from backend import storage_handler as sh
-
-class Model:
-    def __init__(self):
-        self.model()
+import storage_handler as sh
 
 
-    def fit_model(self, model_id, dataset_id):
-        modelconfig = sh.get_model(model_id)
-        layers = []
-        for i in range(0, modelconfig['num_layers'] - 2):
-            layers[i] = tf.keras.layers.Dense(units=modelconfig['units_per_layer'], activation="relu")
-        layers[modelconfig['num_layers'] - 1] = tf.keras.layers.Dense(units=1)
-        self.model = tf.keras.models.sequential(layers)
+def train(user_id, dataset_id, model_id, fingerprint, label, epochs, accuracy, batch_size):
+    dataset = sh.get_dataset(dataset_id)
+    model = sh.get_model(user_id, model_id)
 
-        # managing the access to datasets got moved to separate class,
+    # TODO: implement training (milestone for 26/08)
+    fitting = None
 
-        with open(sh.get_dataset_path(dataset_id), "rb") as f:
-            data = pickle.load(f)
-
-        # data is stored as string, parse it first
-        data = [json.loads(x) for x in data]
-
-        x, y = zip(*[(mol["x"][str(modelconfig['fingerprint_size'])], mol["y"][modelconfig['label']]) for mol in data])
-        x, y = np.array(x), np.array(y)
-
-        self.model.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.MeanSquaredError(),
-                      metrics=[tf.keras.metrics.MeanAbsoluteError()])
-
-        self.model.build(input_shape=x.shape)
-        self.model.summary()
-
-        history = self.model.fit(x,
-                            y,
-                            epochs=modelconfig['epochs'],
-                            batch_size=modelconfig['batch_size'],
-                            verbose=modelconfig['verbose'],
-                            use_multiprocessing=True)
+    sh.add_fitting(user_id, dataset_id, epochs, accuracy, batch_size, model_id, fitting)
 
 
-def make_prediction(model_id, molecule):
-    #get model by modelid
-    result = model.predict(molecule)
-    sh.addNewMolecule(molecule, result)
+def analyze(user_id, fitting_id, molecule_id):
+    fitting = sh.get_fitting(user_id, fitting_id)
+    molecule = molecule_id  # TODO: Convert Molecule
+
+    analysis = fitting.predict(molecule)
+    sh.add_analysis(sh, molecule, fitting, analysis)
