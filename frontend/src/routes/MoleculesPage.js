0581e8ebbec7058ba04194fa216b27ee32bd5cf0
AnoukSommer <anoukjohanna.sommer@gmail.com>
2022-07-27 18:27:05 +0200

    UPD Connected Molecule frontend with backend

--- frontend/src/routes/MoleculesPage.js
+++ frontend/src/routes/MoleculesPage.js
@@ -1,54 +1,24 @@
 import React from 'react'
 import { Card, CardContent, Grid, Box, TextField } from '@mui/material'
 import Button from '@mui/material/Button'
-import Molecule from '../internal/Molecule'
-import MoleculeAnalysis from '../internal/MoleculeAnalysis'
 import SelectionList from '../components/SelectionList'
 import { Link } from 'react-router-dom'
 import { Jsme } from 'jsme-react'
+import UserContext from '../UserContext'
+import api from '../api'
 
 export default function MoleculesPage() {
-  const testMolecules = [
-    new Molecule('molecule_1', 'CC(CC1=CC=CC=C1)NC', [
-      new MoleculeAnalysis('my favourite model', 1, { toxicity: 5000 }),
-      new MoleculeAnalysis('second_model', 0, {
-        power_level: 500,
-        noble_gas: false,
-      }),
-    ]),
-    new Molecule('molecule_2', 'CCC', [
-      new MoleculeAnalysis('my favourite model', 1, { toxicity: 500 }),
-      new MoleculeAnalysis('second_model', 0, {
-        power_level: 500,
-        noble_gas: false,
-      }),
-    ]),
-    new Molecule('molecule_3', 'CNC', [
-      new MoleculeAnalysis('my favourite model', 1, { toxicity: 11616 }),
-      new MoleculeAnalysis('second_model', 0, {
-        power_level: 4,
-        noble_gas: true,
-      }),
-    ]),
-    new Molecule('molecule_4', 'C1=CC=CN', [
-      new MoleculeAnalysis('my favourite model', 0, { toxicity: 124 }),
-      new MoleculeAnalysis('second_model', 6, {
-        power_level: 1,
-        noble_gas: false,
-      }),
-      new MoleculeAnalysis('third_model', 3, {
-        test: 1,
-        other_test: 12512512,
-        other_other_test: `yes but maybe not it's quite weird tbh`,
-        smells_good: false,
-        light: 'no',
-      }),
-    ]),
-    new Molecule('Hydrogen-chloride', 'CCCCCCCC', [
-      new MoleculeAnalysis('second_model', 5, { power_level: 9001 }),
-    ]),
-  ]
+  const [molecules, setMolecules] = React.useState([])
+  const user = React.useContext(UserContext)
 
+  React.useEffect(() => {
+    api.getMoleculeList(user.userID).then((moleculeList) => {
+      setMolecules(moleculeList)
+      console.log(moleculeList)
+    })
+  }, [user])
+
+  // TODO
   let selectedMolecule = testMolecules[0]
 
   let showEditor = true
@@ -69,13 +39,15 @@ export default function MoleculesPage() {
 
   const forceUpdate = useForceUpdate()
 
+  // eslint-disable-next-line no-unused-vars
   return (
     <Box sx={{ m: 5 }}>
       <Grid container spacing={2}>
         <Grid item md={3}>
           {
             <SelectionList
-              elements={testMolecules}
+              elements={molecules}
+              /* Todo enter the userID above */
               elementType="molecule"
               usePopper={true}
               addFunc={() =>
