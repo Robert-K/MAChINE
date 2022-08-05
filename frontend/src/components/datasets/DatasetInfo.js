e3c1943325cab150930f07a1fbbc18a6f1618a18
Viola Buck Cabrera <83520648+Viola2345@users.noreply.github.com>
2022-08-05 11:58:01 +0200

    UPD: Added label selection to dataset popper

--- frontend/src/components/datasets/DatasetInfo.js
+++ frontend/src/components/datasets/DatasetInfo.js
@@ -1,14 +1,40 @@
 import React from 'react'
-import { Divider, List, ListItem, Button } from '@mui/material'
+import {
+  Button,
+  Checkbox,
+  Divider,
+  FormControlLabel,
+  FormLabel,
+  List,
+  ListItem,
+} from '@mui/material'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
 export default function DatasetInfo({ dataset }) {
+  const [labelArray, setLabelArray] = React.useState([])
+
+  const handleChecked = (event) => {
+    // TODO: Vielleicht etwas mehr darauf auslegen, dass man später mehr Label auswählen kann
+    if (labelArray.includes(event.target.value)) {
+      setLabelArray([])
+    } else {
+      setLabelArray([event.target.value])
+    }
+  }
+
   return (
     <List sx={{ maxHeight: 400, overflow: 'auto' }}>
-      <Button fullWidth component={Link} to="/training" variant="contained">
+      <Button
+        fullWidth
+        component={Link}
+        to="/training"
+        variant="contained"
+        sx={{ mb: 2 }}
+      >
         Start Training!
       </Button>
+      <FormLabel sx={{ ml: 0.5 }}>Label</FormLabel>
       {dataset.labelDescriptors.map((descriptor, index) => {
         return (
           <ListItem
@@ -17,7 +43,16 @@ export default function DatasetInfo({ dataset }) {
             }`}
           >
             {index === 0 ? null : <Divider></Divider>}
-            {descriptor}
+            <FormControlLabel
+              control={
+                <Checkbox
+                  value={descriptor}
+                  checked={labelArray.includes(descriptor)}
+                  onClick={handleChecked}
+                />
+              }
+              label={descriptor}
+            />
           </ListItem>
         )
       })}
