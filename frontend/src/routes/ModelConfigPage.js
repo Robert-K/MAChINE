faa3f632ae54cb7516e7f8539b684c9fbb884d29
AnoukSommer <anoukjohanna.sommer@gmail.com>
2022-08-26 11:05:09 +0200

    FIX Dropdowns changed to selectors

--- frontend/src/routes/ModelConfigPage.js
+++ frontend/src/routes/ModelConfigPage.js
@@ -1,113 +1,84 @@
 import React from 'react'
-import Layer from '../internal/Layer'
-import LayerVisual from '../components/LayerVisual'
-// import { useLocation } from 'react-router-dom'
-// import ModelVisual from '../components/ModelVisual'
 import Grid from '@mui/material/Grid'
-import {
-  Collapse,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-} from '@mui/material'
-import ExpandLess from '@mui/icons-material/ExpandLess'
-import ExpandMore from '@mui/icons-material/ExpandMore'
+import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
 
 export default function ModelConfigPage() {
-  // const { state } = useLocation()
-  // const baseModel = state.baseModel
-  /*
-  const [epochs, setEpochs] = React.useState(1000)
-  const handleEpochsChange = (event) => {
-    setEpochs(event.target.value)
-  }
-  const [batchsize, setBatchSize] = React.useState(64)
-  const handlebatchsizeChange = (event2) => {
-    setBatchSize(event2.target.value)
-  }
-  const [numlayers, setnumlayers] = React.useState(4)
-  const handlenumlayersChange = (event4) => {
-    setnumlayers(event4.target.value)
-  }
-  const [unitsperlayer, setunitsperlayer] = React.useState(256)
-  const handleunitsperlayerChange = (event5) => {
-    setunitsperlayer(event5.target.value)
-  }
-  const [label, setlabel] = React.useState('randomLabel.txt')
-  const handlelabelChange = (event7) => {
-    setlabel(event7.target.value)
+  const settableParameters = {
+    Optimizer: ['Adam', 'Adamax', 'Stochastic Gradient Descent'],
+    Loss: [
+      'Mean Squared Error',
+      'Mean Absolute Error',
+      'Huber Loss',
+      'Binary Cross Entropy',
+    ],
   }
 
-  const [answer, setanswer] = React.useState('Controlled')
-  */
-
-  const testLayer = new Layer('Dense', 4)
-
-  const settableParameters = [
-    {
-      name: 'Optimizers',
-      nnFunctions: ['Adam', 'Adamax', 'Stochastic Gradient Descent'],
-    },
-    {
-      name: 'Loss',
-      nnFunctions: [
-        'Mean Squared Error',
-        'Mean Absolute Error',
-        'Huber Loss',
-        'Binary Cross Entropy',
-      ],
-    },
-  ]
-
-  /* const modelconfig = {
-    epochs: '1000',
-    batch_size: '64',
-    verbose: '1',
-    num_layers: '4',
-    units_per_layer: '256',
-    fingerprint_size: '128',
-    label: 'randomLabel.txt',
-  } */
-  const [expand, setExpand] = React.useState(false)
-  const toggleExpand = () => {
-    setExpand(!expand)
+  const [funcs, setFuncs] = React.useState(['', ''])
+  const handleChange = (event, i) => {
+    const funcsClone = [...funcs]
+    funcsClone[i] = event.target.value
+    setFuncs(funcsClone)
   }
 
   return (
     <div>
-      {/* <ModelVisual model={baseModel} /> */}
       <Grid container>
         <Grid item xs={8}>
-          {/* <LayerVisual layer={testLayer} /> */}
+          {/* Placeholder bc we actually want the visualization to be here */}
         </Grid>
         <Grid item xs={4}>
           <React.Fragment key={JSON.stringify(settableParameters.parameter)}>
-            {settableParameters.map((parameter) => {
+            {Object.entries(settableParameters).map(([key, value], i) => {
               return (
-                <ListItemButton key={parameter.name} onClick={toggleExpand}>
-                  {parameter.name}
-                  {expand ? <ExpandLess /> : <ExpandMore />}{' '}
-                </ListItemButton>
+                <Box key={i} sx={{ minWidth: 120, m: 2 }}>
+                  <FormControl required fullWidth>
+                    <InputLabel id="select-function-label">{key}</InputLabel>
+                    <Select
+                      labelId="select-function"
+                      id={i}
+                      value={funcs[i]}
+                      label={key}
+                      onChange={(e) => handleChange(e, i)}
+                    >
+                      {value.map((valueEntry, i) => {
+                        return (
+                          <MenuItem key={valueEntry} value={valueEntry}>
+                            {valueEntry}
+                          </MenuItem>
+                        )
+                      })}
+                    </Select>
+                  </FormControl>
+                </Box>
+
+                /*
+
+                <Box key={i}>
+                  <ListItemButton key={i} onClick={() => toggleExpand(i)}>
+                    {key}
+                    {expand[i] ? <ExpandLess /> : <ExpandMore />}{' '}
+                  </ListItemButton>
+                  <Collapse
+                    in={expand[i]}
+                    timeout="auto"
+                    mountOnEnter
+                    unmountOnExit
+                    orientation="vertical"
+                  >
+                    <List component="div" dense>
+                      {value.map((valueEntry, i) => {
+                        return (
+                          <ListItem sx={{ pl: 4 }} key={i}>
+                            <ListItemText>{valueEntry}</ListItemText>
+                          </ListItem>
+                        )
+                      })}
+                    </List>
+                  </Collapse>
+                </Box>
+                */
               )
             })}
-            <Collapse
-              in={expand}
-              timeout="auto"
-              mountOnEnter
-              unmountOnExit
-              orientation="vertical"
-            >
-              <List component="div" dense>
-                {settableParameters.parameter.map(([key, nnFunction]) => {
-                  return (
-                    <ListItem sx={{ pl: 4 }} key={key}>
-                      <ListItemText>{nnFunction}</ListItemText>
-                    </ListItem>
-                  )
-                })}
-              </List>
-            </Collapse>
           </React.Fragment>
         </Grid>
       </Grid>
