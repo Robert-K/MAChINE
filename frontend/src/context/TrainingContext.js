bbaafce2926aa55d3f823c5b26af85db6afc91ab
Viola Buck Cabrera <83520648+Viola2345@users.noreply.github.com>
2022-08-26 15:55:29 +0200

    UPD: Added default values to TrainingContext

--- frontend/src/context/TrainingContext.js
+++ frontend/src/context/TrainingContext.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
-const TrainingContext = React.createContext()
+const TrainingContext = React.createContext({
+  trainingStatus: false,
+  setTrainingStatus: () => {},
+  selectedModel: null,
+  setSelectedModel: () => {},
+  selectedDataset: null,
+  setSelectedDataset: () => {},
+  selectedLabels: [],
+  setSelectedLabels: () => {},
+})
 
 export const TrainingProvider = TrainingContext.Provider
 export default TrainingContext
