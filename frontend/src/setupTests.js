9db9a156ae35e93f9b501a17c9ae79c980841843
Robert Kossessa <github@kosro.de>
2022-09-05 16:52:02 +0200

    FIX: Frontend testing works!!! Mocking Kekule & suppressing a tsParticles warning (that's normal in testing)

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,52 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+
+// Mock used Kekule classes and functions as it does not load properly in a test environment
+jest.mock('kekule', () => ({
+  Kekule: {
+    Editor: {
+      Composer: jest.fn().mockImplementation(() => ({
+        setPredefinedSetting: jest.fn().mockReturnThis(),
+        setAllowCreateNewChild: jest.fn().mockReturnThis(),
+        setCommonToolButtons: jest.fn().mockReturnThis(),
+        setChemToolButtons: jest.fn().mockReturnThis(),
+        getEditorConfigs: jest.fn().mockImplementation(() => ({
+          structureConfigs: {
+            defBondLength: 1,
+          },
+          hotKeyConfigs: {
+            setHotKeys: jest.fn(),
+          },
+        })),
+        getRenderConfigs: jest.fn().mockImplementation(() => ({
+          colorConfigs: {
+            useAtomSpecifiedColor: true,
+          },
+        })),
+      })),
+    },
+    externalResourceManager: {
+      register: jest.fn(),
+    },
+    ChemWidget: {
+      Viewer: jest.fn().mockImplementation(() => ({
+        setDrawDimension: jest.fn(),
+        setRenderType: jest.fn(),
+        setEnableToolbar: jest.fn(),
+        setToolButtons: jest.fn().mockReturnThis(),
+      })),
+    },
+    Render: {
+      RendererType: {
+        R3D: 'R3D',
+      },
+    },
+  },
+}))
+
+jest.spyOn(global.console, 'warn').mockImplementationOnce((message) => {
+  if (!message.includes('tsParticles pathseg polyfill')) {
+    global.console.warn(message)
+  }
+})
