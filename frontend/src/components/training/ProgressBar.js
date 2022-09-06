0ff6cf15090487c9e8e47f553d8713b8ea69860f
Martin <19313511+CSCMe@users.noreply.github.com>
2022-09-06 18:49:17 +0200

    UPD: Moved api connection from TrainingsPage to TrainingContext
    DEL: TrainingsContext: progress, setProgress

--- frontend/src/components/training/ProgressBar.js
+++ frontend/src/components/training/ProgressBar.js
@@ -3,12 +3,15 @@ import * as React from 'react'
 import TrainingContext from '../../context/TrainingContext'
 
 export default function ProgressBar() {
-  const training = React.useContext(TrainingContext)
+  const { selectedEpochs, trainingData } = React.useContext(TrainingContext)
+  const trainedEpochs = trainingData.epoch
+    ? trainingData.epoch.length * 1.0
+    : 0.0
   return (
     <LinearProgress
       variant="determinate"
       color="inherit"
-      value={training.progress}
+      value={(trainedEpochs / selectedEpochs) * 100.0}
     />
   )
 }
--- frontend/src/context/TrainingContext.js
+++ frontend/src/context/TrainingContext.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import api from '../api'
 
 const TrainingContext = React.createContext({
   trainingStatus: false,
@@ -13,9 +15,82 @@ const TrainingContext = React.createContext({
   setSelectedEpochs: () => {},
   selectedBatchSize: 0,
   setSelectedBatchSize: () => {},
-  progress: 0,
-  setProgress: () => {},
+  trainingData: {},
 })
 
-export const TrainingProvider = TrainingContext.Provider
+export const TrainingProvider = ({ children }) => {
+  const [trainingStatus, setTrainingStatus] = React.useState(false)
+  const [selectedModel, setSelectedModel] = React.useState({})
+  const [selectedDataset, setSelectedDataset] = React.useState({})
+  const [selectedLabels, setSelectedLabels] = React.useState([])
+  const [selectedEpochs, setSelectedEpochs] = React.useState(10)
+  const [selectedBatchSize, setSelectedBatchSize] = React.useState(64)
+  const [trainingData, dispatchTrainingData] = React.useReducer(
+    updateTrainingData,
+    {},
+    initTrainingData
+  )
+
+  React.useEffect(() => {
+    api.registerSocketListener('started', () => {
+      setTrainingStatus(true)
+      dispatchTrainingData({ type: 'reset' })
+    })
+    api.registerSocketListener('update', (data) => {
+      dispatchTrainingData({ type: 'update', payload: data })
+    })
+    api.registerSocketListener('done', () => {
+      setTrainingStatus(false)
+    })
+  }, [])
+
+  function updateTrainingData(trainingData, action) {
+    switch (action.type) {
+      case 'update': {
+        const currentData = { ...trainingData }
+        Object.entries(action.payload).forEach(([dataName, value], index) => {
+          currentData[dataName] =
+            currentData[dataName] === undefined
+              ? [value]
+              : [...currentData[dataName], value]
+        })
+        return currentData
+      }
+      case 'reset':
+        return initTrainingData()
+      default:
+        throw new Error()
+    }
+  }
+
+  function initTrainingData() {
+    return {}
+  }
+
+  return (
+    <TrainingContext.Provider
+      value={{
+        trainingStatus,
+        setTrainingStatus,
+        selectedModel,
+        setSelectedModel,
+        selectedDataset,
+        setSelectedDataset,
+        selectedLabels,
+        setSelectedLabels,
+        selectedEpochs,
+        setSelectedEpochs,
+        selectedBatchSize,
+        setSelectedBatchSize,
+        trainingData,
+      }}
+    >
+      {children}
+    </TrainingContext.Provider>
+  )
+}
+
+TrainingProvider.propTypes = {
+  children: PropTypes.any,
+}
 export default TrainingContext
