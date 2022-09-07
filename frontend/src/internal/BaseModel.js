90deeaea8c1d5ab3ed65b773b9524ed2075a64e8
Iva Andreeva <iva.andreeva.13@gmail.com>
2022-09-07 11:08:14 +0200

    UPD: Frontend representation of base models by replacing mlp-specific attributes with parameters array

--- frontend/src/internal/BaseModel.js
+++ frontend/src/internal/BaseModel.js
@@ -1,20 +1,15 @@
 class BaseModel {
-  constructor(name, id, type, taskType, lossFunction, optimizer, layers) {
+  constructor(name, id, type, taskType, parameters) {
     // String, name for base model
     this.name = name
     // Identifier for base model
     this.id = id
-    // ModelType, describes the base model's type as well as the type's
-    // associated image.
+    // ModelType, describes the base model's type as well as the type's associated image.
     this.type = type
-    // String, describes base model task type, ex; "regression", "classifier"
+    // String, describes base model task type, ex; "regression", "classification"
     this.taskType = taskType
-    // String of loss function used for the model
-    this.lossFunction = lossFunction
-    // String of optimizer used for model
-    this.optimizer = optimizer
-    // array of predefined layers
-    this.layers = layers
+    // js object containing type-specific additional model parameters
+    this.parameters = parameters
   }
 }
 export default BaseModel
