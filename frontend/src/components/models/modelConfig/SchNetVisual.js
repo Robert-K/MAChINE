448d35f221a682625c5cfe08dba1c78f19fae0ae
Iva Andreeva <iva.andreeva.13@gmail.com>
2022-09-23 15:01:11 +0200

    FIX: schnet images not fetched correctly

--- frontend/src/components/models/modelConfig/SchNetVisual.js
+++ frontend/src/components/models/modelConfig/SchNetVisual.js
@@ -5,7 +5,7 @@ import Image from 'mui-image'
 export default function SchNetVisual() {
   return (
     <Card sx={{ height: '85vh', m: 2 }}>
-      <Image src="graph.png" alt="Image of graph"></Image>
+      <Image src="/graph.png" alt="Image of graph"></Image>
     </Card>
   )
 }
