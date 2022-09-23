4ff348675891584aa2effb7862febd6deccf91d1
Max <max@ortius-illingen.de>
2022-09-23 11:56:49 +0200

    UPD some details

--- frontend/src/routes/ScoreboardsPage.js
+++ frontend/src/routes/ScoreboardsPage.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Box, Typography } from '@mui/material'
+import { Box, Paper, Typography } from '@mui/material'
 import { DataGrid } from '@mui/x-data-grid'
 import Api from '../api'
 
@@ -13,128 +13,103 @@ export default function ScoreboardsPage() {
       loaded = true // quick fix to spot infinite requests
     })
   }
+  const fittingColumns = [
+    {
+      field: 'id',
+      headerName: <b>ID</b>,
+      headerAlign: 'center',
+      align: 'center',
+      description:
+        'The ID got created automatically. It is unique and IDentifys a fitting',
+      sortable: false,
+      flex: 4,
+      minWidth: 180,
+    },
+    {
+      field: 'modelID',
+      headerName: <b>Model ID</b>,
+      headerAlign: 'center',
+      align: 'center',
+      type: 'number',
+      flex: 4,
+      minWidth: 180,
+    },
+    {
+      field: 'modelName',
+      headerName: <b>Model Name</b>,
+      headerAlign: 'center',
+      align: 'right',
+      description: 'The Name of the Model chosen by its creator',
+      sortable: false,
+      flex: 3,
+      minWidth: 140,
+    },
+    {
+      field: 'datasetID',
+      headerName: <b>Dataset ID</b>,
+      headerAlign: 'center',
+      align: 'right',
+      type: 'number',
+      flex: 2,
+      minWidth: 90,
+    },
+    {
+      field: 'labels',
+      headerName: <b>Label</b>,
+      headerAlign: 'center',
+      align: 'center',
+      flex: 2,
+      minWIdth: 90,
+    },
+    {
+      field: 'epochs',
+      headerName: <b>Epochs</b>,
+      headerAlign: 'center',
+      align: 'right',
+      flex: 2,
+      minWidth: 70,
+    },
+    {
+      field: 'batchSize',
+      headerName: <b>Batch Size</b>,
+      headerAlign: 'center',
+      align: 'right',
+      flex: 2,
+      minWidth: 85,
+    },
+    {
+      field: 'accuracy',
+      headerName: <b>Accuracy</b>,
+      headerAlign: 'center',
+      align: 'right',
+      flex: 2,
+      minWidth: 70,
+    },
+  ]
+
   return (
     <div align="center">
       <Box sx={{ mx: 5, mb: 5, mt: 2 }}>
-        <Typography sx={{ fontSize: 20 }}>Best Models</Typography>
-        <Box sx={{ maxWidth: 1000 }}>
+        <Typography sx={{ m: 4 }} variant="h4">
+          Best Models
+        </Typography>
+        <Paper sx={{ maxWidth: 1000, m: 5 }}>
           {DataTable(fittingColumns, fittingRows)}
-        </Box>
-
-        <Typography sx={{ mt: 5, fontSize: 20 }}>Best Molecules</Typography>
-        <Box sx={{ maxWidth: 1000 }}>
-          {DataTable(moleculeColumns, moleculeRows)}
-        </Box>
+        </Paper>
       </Box>
     </div>
   )
 }
 
-const fittingColumns = [
-  {
-    field: 'id',
-    headerName: 'ID',
-    headerAlign: 'center',
-    align: 'center',
-    sortable: false,
-    flex: 4,
-    minWidth: 180,
-  },
-  {
-    field: 'modelID',
-    headerName: 'Model ID',
-    headerAlign: 'center',
-    align: 'center',
-    type: 'number',
-    flex: 4,
-    minWidth: 180,
-  },
-  {
-    field: 'modelName',
-    headerName: 'Model Name',
-    headerAlign: 'center',
-    align: 'right',
-    sortable: false,
-    flex: 3,
-    minWidth: 140,
-  },
-  {
-    field: 'datasetID',
-    headerName: 'Dataset ID',
-    headerAlign: 'center',
-    align: 'right',
-    type: 'number',
-    flex: 2,
-    minWidth: 90,
-  },
-  {
-    field: 'labels',
-    headerName: 'Label',
-    headerAlign: 'center',
-    align: 'center',
-    flex: 2,
-    minWIdth: 90,
-  },
-  {
-    field: 'epochs',
-    headerName: 'Epochs',
-    headerAlign: 'center',
-    align: 'right',
-    flex: 2,
-    minWidth: 70,
-  },
-  {
-    field: 'batchSize',
-    headerName: 'Batch Size',
-    headerAlign: 'center',
-    align: 'right',
-    flex: 2,
-    minWidth: 85,
-  },
-  {
-    field: 'accuracy',
-    headerName: 'Accuracy',
-    headerAlign: 'center',
-    align: 'right',
-    flex: 2,
-    minWidth: 70,
-  },
-]
-
-const moleculeColumns = [
-  {
-    field: 'name',
-    headerName: 'Name',
-    sortable: false,
-    width: 120,
-  },
-  {
-    field: 'smiles',
-    headerName: 'Smiles Code',
-    sortable: false,
-    width: 120,
-  },
-  {
-    field: 'cml',
-    headerName: 'CML Code',
-    sortable: false,
-    width: 130,
-  },
-]
-
-let moleculeRows = []
-Api.getMoleculeList().then((data) => {
-  moleculeRows = data
-})
-
 function DataTable(columns, rows) {
   return (
     <div style={{ height: 600, width: '100%' }}>
       <DataGrid
+        sx={{ borderColor: 'primary.light' }}
         rows={rows}
         columns={columns}
-        pageSize={10}
-        hideFooterPagination={true}
+        disableColumnMenu={true}
+        hideFooter={true}
         experimentalFeatures={{ columnGrouping: true }}
       />
     </div>
