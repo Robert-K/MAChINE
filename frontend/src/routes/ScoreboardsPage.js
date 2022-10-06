6f20421f23d27225d653f5ebc400cc284a9360cb
Max <max@ortius-illingen.de>
2022-10-06 17:05:03 +0200

    FIX: delete button alignment in scoreboard

--- frontend/src/routes/ScoreboardsPage.js
+++ frontend/src/routes/ScoreboardsPage.js
@@ -1,14 +1,5 @@
 import * as React from 'react'
-import {
-  Box,
-  Button,
-  Card,
-  IconButton,
-  Stack,
-  Typography,
-  useTheme,
-} from '@mui/material'
-import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings'
+import { Box, Button, Card, IconButton, Stack, Typography } from '@mui/material'
 import DeleteSweepIcon from '@mui/icons-material/DeleteSweep'
 import DeleteIcon from '@mui/icons-material/Delete'
 import { DataGrid } from '@mui/x-data-grid'
@@ -17,21 +8,25 @@ import UserContext from '../context/UserContext'
 import PropTypes from 'prop-types'
 import { camelToNaturalString } from '../utils'
 
+/**
+ * The Scoreboard gets its data via a Api request and the userContext, thus no paramets need to be specified
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export default function ScoreboardsPage() {
   const [fittingRows, setFittingRows] = React.useState([])
   const [highlightedRows, setHighlightedRows] = React.useState([])
 
-  const { adminMode, setAdminMode } = React.useContext(UserContext)
+  const { adminMode } = React.useContext(UserContext)
 
-  const theme = useTheme()
-
+  // defining the order and context of the columns
   const fittingColumns = [
     {
-      field: 'userName',
+      field: 'userName', // the api request delivers an object, the field value is the key to be used
       headerName: 'Username',
       headerAlign: 'center',
       align: 'center',
-      flex: 4,
+      flex: 4, // flex is for scaling, a flex 4 column will be twice as wide as an flex 2 columns
       minWidth: 150,
     },
 
@@ -50,22 +45,41 @@ export default function ScoreboardsPage() {
       headerAlign: 'center',
       align: 'center',
       sortable: false,
+      // a renderCell allows to put html inside a Table cell, like for example a button
       renderCell: (params) => {
         return (
-          <div>
-            {params.id}
-            {adminMode ? (
-              <IconButton
-                onClick={() => {
-                  api.deleteScoreboardFitting(params.id).then((response) => {
-                    refresh()
-                  })
-                }}
-              >
-                <DeleteIcon />
-              </IconButton>
-            ) : null}
-          </div>
+          <Box
+            display="flex"
+            sx={{
+              width: '100%',
+              height: '100%',
+            }}
+          >
+            <Box
+              display="flex"
+              sx={{
+                flexGrow: '1',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <div>{params.id}</div>
+            </Box>
+            <Box display="flex">
+              {adminMode ? (
+                <IconButton
+                  sx={{ alignItems: 'center' }}
+                  onClick={() => {
+                    api.deleteScoreboardFitting(params.id).then((response) => {
+                      refresh()
+                    })
+                  }}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              ) : null}
+            </Box>
+          </Box>
         )
       },
       flex: 4,
@@ -88,6 +102,7 @@ export default function ScoreboardsPage() {
       sortable: false,
       flex: 2,
       minWidth: 90,
+      // capitalize the first letter of each label
       renderCell: (params) => {
         return <div>{camelToNaturalString(params.value.join(', '))}</div>
       },
@@ -110,7 +125,7 @@ export default function ScoreboardsPage() {
     },
     {
       field: 'accuracy',
-      headerName: 'Accuracy',
+      headerName: 'Accuracy(R Squared)',
       headerAlign: 'center',
       align: 'center',
       flex: 2,
@@ -122,6 +137,10 @@ export default function ScoreboardsPage() {
     refresh()
   }, [])
 
+  /**
+   * gets the required info from the backend: all the fittings to be displayed in the Scoreboard and
+   * all fitting of the current use to be able to highlight them
+   */
   function refresh() {
     api.getScoreboardSummaries().then((data) => {
       setFittingRows(data)
@@ -146,9 +165,7 @@ export default function ScoreboardsPage() {
         <Typography sx={{ m: 4 }} variant="h4">
           Best Models
         </Typography>
-        {adminMode ? (
-          <AdminPanel changeFunc={setAdminMode} refreshFunc={refresh} />
-        ) : null}
+        {adminMode ? <AdminPanel refreshFunc={refresh} /> : null}
         <Card sx={{ maxWidth: '80vw', m: 4 }}>
           <DataTable
             columns={fittingColumns}
@@ -161,7 +178,13 @@ export default function ScoreboardsPage() {
   )
 }
 
-function AdminPanel({ changeFunc, refreshFunc }) {
+/**
+ * a hidden panel with extra features that shows up when adminMode == true
+ * @param refreshFunc gets called when the button inside this panel "Delete All!" is clicked
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function AdminPanel({ refreshFunc }) {
   return (
     <Box>
       <Button
@@ -177,17 +200,6 @@ function AdminPanel({ changeFunc, refreshFunc }) {
       >
         Delete All!
       </Button>
-      <Button
-        variant="contained"
-        size="large"
-        sx={{ m: 1 }}
-        onClick={() => {
-          changeFunc(false)
-        }}
-        endIcon={<AdminPanelSettingsIcon />}
-      >
-        Leave Admin Mode
-      </Button>
     </Box>
   )
 }
@@ -197,6 +209,15 @@ AdminPanel.propTypes = {
   refreshFunc: PropTypes.func,
 }
 
+/**
+ * creates a styled table
+ * @param columns array of object that describes the columns
+ * @param rows array of objects that represent a row each, where the keys match the 'field' values stated in columns
+ * @param highlightedRows array of objects just like 'rows', the cut of 'rows' and 'highlightedRows' gets highlighted
+ * @returns {JSX.Element}
+ * @constructor
+ */
+
 function DataTable({ columns, rows, highlightedRows }) {
   return (
     <DataGrid
@@ -205,7 +226,8 @@ function DataTable({ columns, rows, highlightedRows }) {
       columns={columns}
       disableColumnMenu={true}
       hideFooter={true}
-      experimentalFeatures={{ columnGrouping: true }}
+      pageSize={10}
+      // check for each fitting if it's been created by the current user
       getRowClassName={(params) => {
         return highlightedRows.some(({ id }) => id === params.row.id)
           ? 'table-theme'
