5b4021dd36902c391442435211a9565e7c1c774f
AnoukSommer <anoukjohanna.sommer@gmail.com>
2022-10-07 12:01:32 +0200

    UPD: AnalysisInfo.js documentation

--- frontend/src/components/molecules/AnalysisInfo.js
+++ frontend/src/components/molecules/AnalysisInfo.js
@@ -11,6 +11,14 @@ import ExpandMore from '@mui/icons-material/ExpandMore'
 import PropTypes from 'prop-types'
 import { camelToNaturalString } from '../../utils'
 
+/**
+ * This component is used on MoleculesPage in the DetailsPopper (after clicking on the i-icon of a molecule).
+ * It shows everything the user needs to know about a particular analysis of the selected molecule.
+ * The analyzed labels are not inherently shown, the user has to click to expand the analysisInfo and view them.
+ * @param analysis The analysis to be described
+ * @returns {JSX.Element} The name of the model, its ID, and the analyzed labels with their values.
+ * @constructor
+ */
 export default function AnalysisInfo({ analysis }) {
   const [expand, setExpand] = React.useState(false)
   const toggleExpand = () => {
