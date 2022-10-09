09bc0f6dd24e4ead77fe623fcaa34da6287e7bc0
Viola Buck Cabrera <83520648+Viola2345@users.noreply.github.com>
2022-10-09 14:12:45 +0200

    DEL: Unused function

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,22 +1,3 @@
-// Turns the string into a pseudo-random hex color
-export function stringToColor(string) {
-  let hash = 0
-  let i
-
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash)
-  }
-
-  let color = '#'
-
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff
-    color += `00${value.toString(16)}`.slice(-2)
-  }
-
-  return color
-}
-
 // Nothing so see here...
 const pattern = [
   'ArrowUp',
